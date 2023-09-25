921e24d310b3c1da337a8d9c56cc44d47cd4817e
ChoiKT <chlrkdxkr@gmail.com>
2023-09-25 19:59:23 +0900

    apply ssl

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './utils/all.exceptions.filter';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./src/cert/privkey1.pem'),
+    cert: fs.readFileSync('./src/cert/cert1.pem'),
+    ca: fs.readFileSync('./src/cert/fullchain1.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
